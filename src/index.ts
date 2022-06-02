88b760bd440f2914fb4b46e531b794401f05cf20
Anthony Marvin Gomez <amarvin@husldigital.com>
2022-06-02 12:31:07 -0400

    add three

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,125 @@
-import './player'
-import './board'
+import * as THREE from 'three';
 
-const keyDown = (e: { code: string }) => {
-    if( e.code === 'ArrowUp' || e.code === 'ArrowDown' || e.code === 'ArrowLeft' || e.code === 'ArrowRight') {
-        logKey(e.code)
-        if( player ) {
-           movement(player, e.code)
-        }
+import './player';
+import './board';
 
-    }
+// init
 
-    function logKey( key: string ): void {
-        console.log(key)
+const camera = new THREE.PerspectiveCamera(
+	70,
+	window.innerWidth / window.innerHeight,
+	0.01,
+	10
+);
+camera.position.z = 1;
 
-    }
-    return;
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement);
+
+// animation
+
+function animation(time: number) {
+	// console.log(time);
+	mesh.rotation.x = time / 2000;
+	// mesh.rotation.y = time / 1000;
+	// mesh.position.x = time / 1000;
+	// keyDown(e);
+	renderer.render(scene, camera);
 }
 
-const newPlayer = document.createElement( 'div' )
-newPlayer.classList.add('player')
+// const keyDown = (e: { code: string }) => {
+// 	if (
+// 		e.code === 'ArrowUp' ||
+// 		e.code === 'ArrowDown' ||
+// 		e.code === 'ArrowLeft' ||
+// 		e.code === 'ArrowRight'
+// 	) {
+// 		movement(mesh, e.code);
+// 	}
 
-document.querySelector('.gameboard').appendChild(newPlayer)
+// 	function logKey(key: string): void {
+// 		console.log(key);
+// 	}
+// 	return;
+// };
 
-const player = document.querySelector( '.player' );
+// // const newPlayer = document.createElement('div');
+// // newPlayer.classList.add('player');
 
+// // document.querySelector('.gameboard').appendChild(newPlayer);
 
-//TODO #2:
-function movement(item: any, arrowCode: string): void {
-    let element = item.getBoundingClientRect();
-    let x,y;
-    let bod = document.querySelector( '.gameboard' );
-    let bodSize = bod.getBoundingClientRect();
-    if( element && x == undefined && y == undefined ) {
-        x = element.left
-        y = element.top
-     }
+// const player = document.querySelector('.player');
 
-    // console.log( arrowCode)
-    if( arrowCode === undefined || item === undefined ) return
-    if( arrowCode === 'ArrowUp' ) {
-        y = y - 16
-    }
-    if (arrowCode === 'ArrowDown') {
-        y = y + 16
-    }
-    if( arrowCode === 'ArrowLeft') {
-        x = x - 16
-    }
-    if( arrowCode ===  'ArrowRight') {
-        x = x + 16
-    }
-    if( x < 8 || y < 8 ) {
-        return;
-    }
-    if( arrowCode === 'ArrowRight' || arrowCode === 'ArrowLeft' ) {
-        if( x < bodSize.width ) {
-            item.style.left = `${x}px`
-        }
+// //TODO #2:
+// function movement(item: any, arrowCode: string): void {
+// 	let element = item;
+// 	let x, y;
+// 	let bod = document.querySelector('body');
+// 	let bodSize = bod.getBoundingClientRect();
+// 	if (element && x == undefined && y == undefined) {
+// 		x = element.position.x;
+// 		y = element.position.y;
+// 	}
 
-    }
+// 	// console.log( arrowCode)
+// 	if (arrowCode === undefined || item === undefined) return;
+// 	if (arrowCode === 'ArrowUp') {
+// 		y = y - 16;
+// 	}
+// 	if (arrowCode === 'ArrowDown') {
+// 		y = y + 16;
+// 	}
+// 	if (arrowCode === 'ArrowLeft') {
+// 		x = x - 16;
+// 	}
+// 	if (arrowCode === 'ArrowRight') {
+// 		x = x + 16;
+// 	}
+// 	if (x < 8 || y < 8) {
+// 		return;
+// 	}
+// 	if (arrowCode === 'ArrowRight' || arrowCode === 'ArrowLeft') {
+// 		if (x < bodSize.width) {
+// 			item.position.x = x;
+// 			renderer.render(scene, camera);
+// 		}
+// 	}
 
-    if( arrowCode === 'ArrowDown' || arrowCode === 'ArrowUp' ) {
-        if( y < bodSize.height ) {
-            item.style.top = `${y}px`
-        }
+// 	if (arrowCode === 'ArrowDown' || arrowCode === 'ArrowUp') {
+// 		if (y < bodSize.height) {
+// 			item.position.y = y;
+// 			renderer.render(scene, camera);
+// 		}
+// 	}
 
-    }
+// 	console.log(x, y);
+// }
+// // function makeSquares() {
+// // 	console.log(window.innerWidth, window.innerHeight);
+// // 	let bod = document.querySelector('body');
+// // 	let bodSize = bod.getBoundingClientRect();
+// // 	let boundaries = 16 * 16;
+// // 	for (let i = 0; i < boundaries; i++) {
+// // 		// console.log(i)
+// // 		let bod = document.querySelector('.gameboard');
+// // 		let square = document.createElement('div');
+// // 		square.classList.add('square');
+// // 		if (!bod) return;
+// // 		bod.appendChild(square);
+// // 	}
+// // }
 
-    console.log(x,y)
-}
-function makeSquares() {
-    console.log( window.innerWidth, window.innerHeight);
-    let bod = document.querySelector( 'body' );
-    let bodSize = bod.getBoundingClientRect()
-    let boundaries =  16 * 16;
-    for( let i = 0; i < boundaries; i++ ) {
-        // console.log(i)
-        let bod = document.querySelector( '.gameboard' );
-        let square = document.createElement( 'div' );
-        square.classList.add( 'square')
-        if (!bod) return;
-        bod.appendChild( square );
-    }
-
-
-}
-
-document.addEventListener( 'keydown', keyDown, false);
-window.addEventListener( 'load', () => {
-    makeSquares()
-})
+// document.addEventListener('keydown', keyDown, false);
+// window.addEventListener('load', () => {
+// 	// makeSquares();
+// });
